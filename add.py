e0b173b68832840d8307677fe29606fbbb76a391
Berger Matthieu <matthieu.berger.27430@gmail.com>
2025-06-13 11:31:53 +0200

    je pleure

--- add.py
+++ add.py
@@ -1,3 +1,6 @@
 def plus_one(num):
     num += 1
     return num
+
+test = plus_one(1)
+print(test)
--- test_add.py
+++
@@ -1,6 +0,0 @@
-from main import plus_one
-
-def test_plus_one():
-    assert plus_one(1) == 2
-    assert plus_one(0) == 1
-    assert plus_one(-1) == 0
